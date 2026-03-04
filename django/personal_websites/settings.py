131de6ad6088e4f3f89ab7aa381e4e4480fa84bd
Show530 <sophiahowson@gmail.com>
2026-03-04 13:50:31 -0500

    Backend- switched to use python-decouple library

--- django/personal_websites/settings.py
+++ django/personal_websites/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 from datetime import timedelta # for oauth
-from dotenv import load_dotenv
+# old/new
+# from dotenv import load_dotenv
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-load_dotenv()  # reads .env file
+# load_dotenv()  # reads .env file
 
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+# SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = config('DJANGO_SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
